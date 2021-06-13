748580faa39435c9c3bc325139ac3a1471de8c1a
timur <matskevich-06@mail.ru>
2021-06-13 14:47:06 +0400

    not full commit

--- javascript.js
+++ javascript.js
@@ -4,10 +4,15 @@ var name = 'text'; //text variable
 var bool = true; //boolean variable
 console.log(bool); //show number in console
 
-var username = prompt("What's your name?"); //input data from user
-console.log('Hello, ' + username); //merge two strings
+/*var username = prompt("What's your name?"); //input data from user
+console.log('Hello, ' + username); //merge two strings*/
 
 /* It's
 a
 multi-string
 comment */
+
+firstNum = 58; //two number variables
+secondNum = 47;
+document.write('The result is ' + firstNum + secondNum); //this would just merge strings, not count the result
+resultNum = firstNum + secondNum; //to add one number to another 
