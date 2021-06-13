29631fef22979164bd59b5042f51a85e524e9ba0
timur <matskevich-06@mail.ru>
2021-06-13 13:11:35 +0400

    boolean variable

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 var num = 51.5; //number variable
 var Num = 25; //register matters for js, it's another variable
 var name = 'text'; //text variable
-console.log(num); //show number in console
+var bool = true; //boolean variable
+console.log(bool); //show number in console
