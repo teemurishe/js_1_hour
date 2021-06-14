379e34b0732950097770da7702664c7f474e9a91
teemurishe <matskevich-06@mail.ru>
2021-06-14 15:29:20 +0400

    short maths operations

--- javascript.js
+++ javascript.js
@@ -15,3 +15,13 @@ comment */
 firstNum = 58; //two number variables
 secondNum = 47;
 document.write('The result is ', firstNum + secondNum); //now numbers are added
+firstNum = firstNum + 52; //adding a number to variable
+console.log(firstNum);
+firstNum += 12; //short way to add number to variable
+console.log(firstNum);
+firstNum -= 64; //same for subtract operation
+firstNum +=1; //adding 1 to firstNum with the way described before
+firstNum ++; //another way to add 1
+console.log(firstNum);
+firstNum --; //same for sabtracting 1
+console.log(firstNum);
